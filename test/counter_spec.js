34480f1cc8cf64cc0fc8d668e411b65c3c7471bf
angus croll <angus@twitter.com>
2014-02-02 13:43:53 -0800

    reporters: dots and make one fail

--- test/counter_spec.js
+++ test/counter_spec.js
@@ -13,7 +13,7 @@ describe("counter", function() {
     it("should return the counter", function() {
       expect(counter.analyze(text)).toEqual(counter);
     });
-    iit("should register unique words once", function() {
+    it("should register unique words once", function() {
       counter.analyze(text, 1, /./);
       var grandmereArray = counter.data.words.filter(function(e) {return e[0] == 'grand-mère'})[0];
       expect(grandmereArray[1]).toEqual(1);
@@ -25,7 +25,7 @@ describe("counter", function() {
     });
     it("should filter by simple regex", function() {
       counter.analyze(text, 5, /râc/);
-      expect(counter.data.total).toEqual(1);
+      expect(counter.data.total).toEqual(2);
     });
     it("should filter by positional regex", function() {
       counter.analyze(text, 5, /nd$/);
